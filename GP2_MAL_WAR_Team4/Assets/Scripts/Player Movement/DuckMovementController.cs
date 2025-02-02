3b3c2993bcfe03b27dbcfd2a4048632594c81fb4
Andreas <152264387+Andrea21xd@users.noreply.github.com>
2025-02-02 22:32:32 +0100

    shit

--- GP2_MAL_WAR_Team4/Assets/Scripts/Player Movement/DuckMovementController.cs
+++ GP2_MAL_WAR_Team4/Assets/Scripts/Player Movement/DuckMovementController.cs
@@ -25,7 +25,7 @@ public class DuckMovementController : MonoBehaviour
 
     void Update()
     {
-        HandleIsometricMovement();
+        HandleMovement();
     }
 
     private void HandleMovement()
